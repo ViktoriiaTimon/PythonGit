7d8ca04f4c25dc9e2740f04051aeb9b951f45400
Viktoriia Timon <viktoriiatimon@MacBook-Air-Viktoriia.local>
2024-09-22 13:39:12 +0200

    add new info

---
+++ Git1.py
@@ -0,0 +1,9 @@
+# task 01 == Виправте синтаксичні помилки
+print("Hello", end = " ")
+print("world!")
+
+# task 02 == Виправте синтаксичні помилки
+hello = "Hello"
+world = "world"
+if True:
+    print(f"{hello} {world}!")
